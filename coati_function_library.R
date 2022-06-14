855855451075de56f8a33d9a93013624655d4f25
Emily <egrout@ab.mpg.de>
2022-06-14 15:33:58 +0200

    saving new functions

--- coati_function_library.R
+++ coati_function_library.R
@@ -1,5 +1,11 @@
 #library of general functions
 
+#LIBRARIES
+library(dbscan)
+library(rgdal)
+library(lubridate)
+
+
 #LAT/LON TO UTM CONVERSIONS (AND VICE VERSA)
 #Converts a matrix of lons and lats (lons first column, lats second column) to UTM
 #Inputs:
@@ -59,4 +65,83 @@ utm.to.latlon <- function(EastNorths,LonsCol1=TRUE,utm.zone = '34',southern_hemi
   } else{
     return(LonLats)
   }	
+}
+
+
+#This function computes information about subgroup membership over time
+#Inputs:
+# R [numeric]: radius for DBSCAN
+# xs, ys [n_inds x n_times matrix]: x and y locations of individuals 
+#Outputs:
+# subgroup_data: object that contains:
+#   $ind_subgroup_membership [n_inds x n_times matrix]: matrix giving the subgroup id for each individual at each time
+#   $n_subgroups [n_times vector]: vector of the number of subgroups over time
+#   $subgroup_counts [max_n_subgroups x n_times matrix]: matrix giving the number of individuals in each subgroup (with NAs when there are fewer subgroups than the max)
+#   $R [numeric]: radius used in DBSCAN
+get_subgroup_data <- function(xs, ys, R){
+  
+  
+  sub_groups <- matrix(NA, nrow = n_inds, ncol = n_times)
+  
+  for (t_idx in 1:n_times){
+    
+    #getting the x and y coordinates for each time point
+    x <- xs[ , t_idx]
+    y <- ys[, t_idx]
+    
+    non_nas <- which(!is.na(x))
+    
+    x_non_na <- x[non_nas]
+    y_non_na <- y[non_nas]
+    
+    n_tracked <- length(non_nas)
+    
+    if(n_tracked >= 1){
+      #scan for each time column to get the cluster number for each individual
+      groups <- dbscan(x= cbind(x_non_na, y_non_na), eps = R, minPts = 1)
+      #saving the cluster for each time into the matrix
+      sub_groups[non_nas, t_idx] <- groups$cluster
+    }
+    
+  }
+  
+  #get max number of groups per time
+  ngroups <- suppressWarnings(apply(sub_groups, 2, max, na.rm = T)) #suppress warnings because if all NAs it gives a warning, this is fine because we deal with it in the next step by removing the infinite maxes
+  #remove infinite values
+  ngroups[is.infinite(ngroups)] <- NA
+  hist(ngroups)
+  plot(ngroups, type = "l")
+  
+  
+  #now want to look at size of the sub-groups as the histogram shows 2 subgroups being most frequent 
+  #but this might be because of one individual not being with the group
+  
+  group_max <- max(ngroups, na.rm = T)
+  
+  subgroup_counts <- matrix(NA, nrow = group_max, ncol = n_times)
+  
+  #for loop for getting the number of individuals in each sub group every 10 mins (when the radius for defining within group is 50m, there are maximum 5 groups in full dataset)
+  for(i in 1:n_times){
+    
+    #going through each time stamp
+    current_group <- sub_groups[, i]
+    #number of individuals in each subgroup 
+    counts <- table(current_group)
+    #to remove NA's when there is no data from all individuals
+    if(length(counts) >= 1){
+      #putting the data into the matrix
+      subgroup_counts[1:length(counts),i] <- sort(counts)
+      
+    }
+  }
+  
+  #store all data in a list
+  subgroup_data <- list()
+  subgroup_data$ind_subgroup_membership <- sub_groups
+  subgroup_data$n_subgroups <- ngroups
+  subgroup_data$subgroup_counts <- subgroup_counts
+  subgroup_data$R <- R
+  
+  return(subgroup_data)
+  
 }
--- fission_fusion.R
+++ fission_fusion.R
@@ -11,76 +11,30 @@
 #are these two things related? does the within group network correlate with the fission-fusion subgroup network
 
 
-library(dbscan)
-
 #--------PARAMS-------
 data_dir <- "C:/Users/egrout/Dropbox/coatithon/processed/2022/"
+code_dir <- 'C:/Users/egrout/Dropbox/coatithon/coatithon_code/'
 in_file <- "galaxy_xy_10min_level0.RData"
 
 #radius for group membership
 R <- 50
 
-#-------MAIN-------
-setwd(data_dir)
+#-------SETUP-------
 
+#read in library of functions
+setwd(code_dir)
+source('coati_function_library.R')
+
+#load data
+setwd(data_dir)
 load(in_file)
+
+#-----MAIN------
 
 n_inds <- nrow(xs)
 n_times <- ncol(xs)
 
-sub_groups <- matrix(NA, nrow = n_inds, ncol = n_times)
-
-for (t_idx in 1:n_times){
-
-  #getting the x and y coordinates for each time point
-  x <- xs[ , t_idx]
-  y <- ys[, t_idx]
-  
-  non_nas <- which(!is.na(x))
-  
-  x_non_na <- x[non_nas]
-  y_non_na <- y[non_nas]
-  
-  n_tracked <- length(non_nas)
-  
-  if(n_tracked >= 1){
-    #scan for each time column to get the cluster number for each individual
-    groups <- dbscan(x= cbind(x_non_na, y_non_na), eps = R, minPts = 1)
-    #saving the cluster for each time into the matrix
-    sub_groups[non_nas, t_idx] <- groups$cluster
-  }
-
-}
-
-#get max number of groups per time
-ngroups <- apply(sub_groups, 2, max, na.rm = T)
-#remove infinite values
-ngroups[is.infinite(ngroups)] <- NA
-hist(ngroups)
-plot(ngroups, type = "l")
-
-
-#now want to look at size of the sub-groups as the histogram shows 2 subgroups being most frequent 
-#but this might be because of one individual not being with the group
-
-group_max <- max(ngroups, na.rm = T)
-
-subgroup_counts <- matrix(NA, nrow = group_max, ncol = n_times)
-
-#for loop for getting the number of individuals in each sub group every 10 mins (when the radius for defining within group is 50m, there are maximum 5 groups in full dataset)
-for(i in 1:n_times){
-  
-  #going through each time stamp
-  current_group <- sub_groups[, i]
-  #number of individuals in each subgroup 
-  counts <- table(current_group)
-  #to remove NA's when there is no data from all individuals
-  if(length(counts) >= 1){
-    #putting the data into the matrix
-    subgroup_counts[1:length(counts),i] <- sort(counts)
-  
-  }
-}
+subgroup_data <- get_subgroup_data(xs, ys, 10)
 
 
 
